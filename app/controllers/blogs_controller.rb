4323ebe4ab2958c2d2961be77d58ddc3dd6ed990
Nathanstuff <n.odonnell@hotmail.com>
2020-10-24 15:45:06 +1100

    create a new blog post

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,23 @@
 class BlogsController < ApplicationController
     def new
     end
-    
+
+    def index
+    end
+
     def create
+        @blogs = Blogpost.new(blog_params)
+        # @blog = Blogpost.new(params[:blog])
+        @blogs.save
+        redirect_to welcome_index_path
+    end
+
+    def show
+        @blogs = Blogpost.find(params[:id])
+    end
+
+    private
+    def blog_params
+        params.require(:blog).permit(:title, :text)
     end
 end
